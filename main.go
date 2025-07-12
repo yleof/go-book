41b8038cfa7400e8296e8230b96cdebbbc57b42f
Yiming Feng <y255feng@uwaterloo.ca>
2025-07-12 15:40:23 -0400

    init

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/books", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode("Hello, Books!")
+	})
+
+	log.Println("API is running")
+	http.ListenAndServe(":4000", router)
+}
